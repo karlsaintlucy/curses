e5d8b6206a7df764e46a3dbf0d4300a33540362b
Karl Saint Lucy <12864843+karlsaintlucy@users.noreply.github.com>
2025-10-30 08:43:40 -0400

    Started 01: Intro and Hello World.

---
+++ hammond_tutorial/01_intro_and_hello_world/main.py
@@ -0,0 +1,10 @@
+# pylint: disable=C0114,C0116
+# https://www.youtube.com/watch?v=4s_wHUNCHK0&list=PL1H1sBF1VAKXLJ3cHisqjy4nGYDMqYIzo&index=2
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
